f20ca5aa7cc3100f3c1af86a59fe638cd68c5250
Shaun Williams <shaunewilliams@gmail.com>
2013-12-15 13:18:23 -0600

    added simple intro eye lid for menu

--- src/scene_menu.js
+++ src/scene_menu.js
@@ -1,7 +1,4 @@
-Blind.scene_title = (function(){
-
-	var eye, title;
-	var script;
+Blind.scene_menu = (function(){
 
 	var lid = (function() {
 		var value, target;
@@ -36,48 +33,42 @@ Blind.scene_title = (function(){
 			draw: draw,
 		};
 	})();
-	
+
 	function init() {
 		lid.reset();
 		lid.open();
-		eye = Blind.assets.images["eye"];
-		title = Blind.assets.images["title"];
-		script = new Blind.TimedScript([
-			{
-				time: 2,
-				action: function() {
-					lid.close();
-				},
-			},
-			{
-				dt: 1.5,
-				action: function() {
-					Blind.setScene(Blind.scene_menu);
-				},
-			},
-		]);
+
+		map = new Blind.Map(Blind.assets.json["map_title"]);
+		Blind.camera.init(map);
+		Blind.camera.disableViewKeys();
+		Blind.camera.disableMoveKeys();
+		Blind.camera.enableProjKeys();
 	}
 
-	function update(dt) {
-		lid.update();
-		script.update(dt);
+	function cleanup() {
+		Blind.camera.disableViewKeys();
+		Blind.camera.disableMoveKeys();
+		Blind.camera.disableProjKeys();
 	}
 
 	function draw(ctx) {
-		var w = Blind.canvas.width;
-		var h = Blind.canvas.height;
 		ctx.fillStyle = "#222";
-		ctx.fillRect(0,0,w,h);
+		ctx.fillRect(0,0,Blind.canvas.width, Blind.canvas.height);
 
-		ctx.drawImage(eye,w/2-eye.width/2,h/2-eye.height/2);
-		ctx.drawImage(title,w/2-title.width/2,h/2-title.height/2);
+		Blind.camera.draw(ctx);
 
 		lid.draw(ctx);
 	}
 
+	function update(dt) {
+		Blind.camera.update(dt);
+		lid.update(dt);
+	}
+	
 	return {
 		init: init,
-		update: update,
+		cleanup: cleanup,
 		draw: draw,
+		update: update,
 	};
 })();
