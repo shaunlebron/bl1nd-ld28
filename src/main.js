016f755fc7a9b6b316afb3e3c55b015ecf457d6e
Shaun Williams <shaunewilliams@gmail.com>
2013-12-15 19:55:05 -0600

    make game start proper

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ window.addEventListener('load', function() {
 
 	Blind.assets.load(function(){
 		Blind.input.init();
-		Blind.setScene(Blind.scene_game1);
+		Blind.setScene(Blind.scene_blare);
 		Blind.executive.start();
 	});
 });
